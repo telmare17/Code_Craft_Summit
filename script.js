eeceb83f39306fa2217d1ad335d8ba3c29b379fd
Telma Reis <telmasreis0@gmail.com>
2025-02-23 17:58:25 -0300

    Javascript

--- script.js
+++ script.js
@@ -1,23 +1,105 @@
-const app = document.getElementById("app");
+const app = document.getElementById("app")
 
-const formAction = () => { 
-    const form = document.getElementById('form');
+// array, vetor
+const users = [
+    {
+        email: "test@test.com",
+        phone: "99999999999",
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: "tust@tust.com",
+        phone: "99999999999",
+        ref: 200,
+        refBy: 100
+    },
+    {
+        email: 'tost@tost.com',
+        phone: '99999999999',
+        ref: 300,
+        refBy: 200
+    }
+]
+
+const getUser = (userData) => {
+    return users.find((user) => {
+      return user.email == userData.email
+    })
+  }
+  
+  const getTotalSubscribers = (userData) => {
+    const subs = users.filter((user) => {
+      return user.refBy == userData.ref
+    })
+    return subs.length
+  }
+
+const showInvite = (userData) => {
+    app.innerHTML = `
+    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+
+    <div id="stats">
+        <h4>
+           ${getTotalSubscribers(userData)}
+        </h4>
+
+        <p>
+            Inscrições feitas!
+        </p>
+    </div>
+    `
+}
+
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() * 4000),
+        refBy: 100
+    }
+
+    users.push(newUser)
+    console.log(users)
+    return newUser
+}
+
+
+const formAction = () => {
+    const form = document.getElementById("form")
     form.onsubmit = (event) => {
         event.preventDefault()
-}
+        const formData = new FormData(form)
+        const userData = {
+            email: formData.get("email"),
+            phone: formData.get("phone"),
+        }
+
+        const user = getUser(userData)
+        if (user) {
+            showInvite(user)
+        } else {
+            const newUser = saveUser(userData)
+            showInvite(newUser)
+        }
+    }
 }
 
 const startApp = () => {
     const content = `
     <form id="form">
-            <input type="email" name="name" placeholder="E-mail"></input>
-            <input type="text" name="phone" placeholder="Telefone"></input>
-            <button>Confirmar</button>
-        </form>
-    `
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="text" name="phone" placeholder="Telefone">
 
-    app.innerHTML = content
+        <button>
+            Confirmar
+        </button>
+    </form>
+    `
+    app.innerHTML = content;
+
     formAction()
 }
-
 startApp()
+
+
+document.getElementById("logo").onclick = () => startApp()
